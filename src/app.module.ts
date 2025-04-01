515b4c01d29ec498f33ab2e3e2cacc1cdc0508e3
shohsulton03 <shohsulton0303@mail.ru>
2025-04-01 12:36:04 +0500

    first

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ envFilePath: ".env", isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      synchronize: true,
+      entities: [join(__dirname, "**/*.entity.{ts,js}")],
+      logging: true,
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cookieParser from "cookie-parser";
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+async function start() {
+  try {
+    const PORT = process.env.PORT || 3030;
+    console.log(PORT);
+    const app = await NestFactory.create(AppModule);
+    app.use(cookieParser());
+    app.useGlobalPipes(new ValidationPipe());
+    app.setGlobalPrefix("api");
+
+    app.enableCors({
+      origin: (origin, callback) => {
+        const allowedOrigins = [
+          "http://localhost:5173",
+          "http://localhost:5174",
+          "http://localhost:3001",
+          "http://localhost:3000",
+          "http://45.92.173.150:3001",
+        ];
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new BadRequestException("Not allowed by CORS"));
+        }
+      },
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true,
+    });
+
+    const config = new DocumentBuilder()
+      .setTitle("Men Stylish API")
+      .setDescription("Men Stylish API")
+      .setVersion("1.0")
+      .addTag(
+        "NESTJS, Validation, swagger, guard, pg, cookie, Telegram Bot, typeorm"
+      )
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api/docs", app, document);
+
+    await app.listen(PORT, () => {
+      console.log(`Server started at:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+start();
